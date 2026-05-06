06ffd23c36693b619a9b21ab369f60f2f14caeef
Aftab Farhan <aftabfarhan324@gmail.com>
2026-05-06 21:21:38 +0600

    set

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,10 @@
-import ArchitectureProgress from "@/components/homepage/ArchitectureProgress";
-import CountDownTrust from "@/components/homepage/countdown";
-import CourseSection from "@/components/homepage/coursSection";
-import HomeHero from "@/components/homepage/hero";
-import LiveInsight from "@/components/homepage/liveInsight";
-import PrecisionWorkflow from "@/components/homepage/workFlow";
 import Image from "next/image";
+import Main_Index_For_Call_Files from "../main";
 
 export default function Home() {
   return (
     <div>
-      <HomeHero />
-      <CountDownTrust />
-      <PrecisionWorkflow />
-      <CourseSection />
-      <ArchitectureProgress />
-      <LiveInsight />
+      <Main_Index_For_Call_Files />
     </div>
   );
 }
